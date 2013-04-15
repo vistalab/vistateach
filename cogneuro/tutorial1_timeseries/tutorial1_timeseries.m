974b69cb2c2ef2787ca7ca7362693d6b60114138
jyeatman <jyeatman@stanford.edu>
2013-04-15 09:57:48 -0700

    updated many things to make it cleaner

--- cogneuro/tutorial1_timeseries/tutorial1_timeseries.m
+++ cogneuro/tutorial1_timeseries/tutorial1_timeseries.m
@@ -22,11 +22,13 @@
 % stimuli and task are manipulatd. Each brain volume is a 3 dimensional
 % image and each pixel in the image is called a voxel. Here is a video of a
 % single slice of the brain over the course of an fMRI experiment where the
-% subject was presented either words or scrambled words.
+% subject was presented either words or scrambled words. Brain volumes were
+% collected every 2 seconds (TR = 2seconds).
 
+% The data is saved in the same folder as the tutorial
 load data
 
-%% Open a figure window
+% Open a figure window
 figure; colormap('gray')
 % Make a movie of slice number 10
 for ii = 1:size(data,4)
@@ -36,40 +38,45 @@ for ii = 1:size(data,4)
     drawnow; pause(.1);
 end
 
-%% View a single time series
+%% View the time series from a single voxel
 
 % While it may not look like there is substantial change in pixel intensity
 % over time if we extract the time series from a voxel in the visual cortex
 % We can see that there is fluctuation in the signal over time. This is due
-% to the blood oxygen level dependent contrast in these images
+% to the blood oxygen level dependent (BOLD) contrast in these images
 
-% BW asks - Why this location?  Do we know it is a good one for this
-% experiment? It looks to me to be in primary visual cortex.
+% Extract the time series from a voxel (x=65, y=53,z=10). 
+% (The function squeeze converts the 4D data, (x,y,z,time) into a vector)
+ts1 = squeeze(data(65,53,10,:));
 
-% Extract the time series from a voxel (x=65, y=45,z=10). 
-% (The function squeeze converts the 4D data, (x,y,z,t) into a vector)
-ts1 = squeeze(data(65,45,10,:));
-nTR = size(data,4);   % Number of brain volumes at the repetition rate
+% This is the number of brain volumes at the repetition rate (TR)
+nTR = size(data,4);   
 
 % Plot the time series for this single voxel.
 figure; 
-plot(1:nTR,ts1)
+plot(1:nTR, ts1)
 xlabel('TR')
 ylabel('Scanner digital value')
+title('Voxel 65,53,10 time series')
 
 % Questions:
 %
 % 1. What are the units of the two axes?
-% 2. Replot the time series with the x axis labeled in seconds. The 
-% function xlabel(' ') will add a label to the axis
+% 2. Find 1 voxel where the signal varies more over time and another voxel
+% where the signal varies less over time. Plot each voxel's time series and
+% title the figure apropriately. Save these as pdfs and turn them in.
 
-%% 
-% These are the times when each event started. Each event lasts
-% 12 seconds and there is a blank screen in between events. The timing is
-% with respect to the fMRI volume number. For example 4 means that the
-% event started at during the acquisition of volume number 4.
-events_words    = [12 21 41 61 86 95];
-events_scramble = [4 32 52 69 77 104];
+%% Associate the time series with the stimuli
+% These are the times when each event started. Each event lasts 12 seconds
+% and there is a blank screen in between events. The timing is with respect
+% to the fMRI volume number. For example 4 means that the event started at
+% during the acquisition of volume number 4 (which is 8 seconds into the
+% experiment because volumes are collected every 2 seconds. We mark each
+% volume during which the event occured meaning that there are a block of
+% six volumes devoted to each occurance of the stimulus
+
+events_words    = [12:17 21:26 41:46 61:66 86:91 95:100];
+events_scramble = [4:9 32:37 52:57 69:74 77:82 104:109];
 
 % Now that we have our event times expressed in terms of scan number we can
 % make an ideal time series that would reflect the expected response
@@ -77,23 +84,23 @@ events_scramble = [4 32 52 69 77 104];
 % express this as a matrix with 2 columns, column 1 containing the
 % predicted time series for words and column 2 containing the predicted time
 % series for scrambled words. There will be 114 rows in the matrix because
-% there were 114 volumes collected in the fMRI experiment.
+% there were 114 volumes collected in the fMRI experiment. This is called
+% the design matrix because it denotes the experimental design.
 
-% First we allocate a matrix full of zeros
+% First we allocate a 114x2 matrix full of zeros
 X = zeros(114,2); 
-% In the first column denote when words were presented.
+% In the first column denote when words were presented by putting a 1 in
+% each location where the word stimulus was on
 X(events_words,1) = 1;
 % In the second column denote when scrambled words were presented
 X(events_scramble,2) = 1;
 
-% The two mage shows two columns of the design matrix
-% The column on the left shows the moment when a volume of the brain is
-% collected and a word is presented.  The column on the right shows the
-% volumes when a scrambled word was presented.
+% The image shows two columns of the design matrix
 
 figure; 
 % These are the times when a word (red) or a scrambled word (blue) are
-% presented
+% presented. The subplot command allows you to put multiple plots in a
+% figure window
 subplot(2,1,1)
 plot(X(:,1),'-r'); hold on
 plot(X(:,2),'-b')
@@ -102,11 +109,17 @@ xlabel('Volume Number'); ylabel('Signal')
 
 % The times for the words and scrambled words are marked in the two columns
 % of this image.  The two columns form a matrix, X, called the design
-% matrix.
+% matrix. One column shows the moment when a volume of the
+% brain is collected and a word is presented.  The other
+% shows the volumes when a scrambled word was presented.
+
 subplot(2,1,2)
-imagesc(X); 
-colormap('gray'); ylabel('Volume Number'); 
-set(gca, 'xtick',[1 2],'xticklabel', {'words' 'scramble'});
+% The ' symbol transposes the matrix X. We are only doing this so that the
+% 2 plots have a common x axis. After transposing the design matrix the x
+% axis is time  and the y axis is condition rather than vice versa. 
+imagesc(X'); 
+colormap('gray'); xlabel('Volume Number'); 
+set(gca, 'ytick',[1 2],'yticklabel', {'words' 'scramble'});
 
 
 %% Analyze the time series
@@ -137,17 +150,13 @@ plot(hrf); xlabel('Scans (2 seconds)');
 
 % The digital values coming from the scanner have no physical significance.
 % digital values from some regions are a little higher than others for
-% instrumental reasons, say because the coil is more sensitive to the
-% surface of the brain than the middle.  Other uninteresting reasons also
-% give rise to these differences.
-
+% instrumental reasons, say because the measurement coil is more sensitive
+% to the surface of the brain than the middle.  Other uninteresting reasons
+% also give rise to these differences.
+%
 % It is common, therefore, to express the time series as a percent
 % modulation around the mean signal. This moves us towards comparing more
 % similar variations - though even this move is not really enough.
-
-
-% BW asks:  The hrf is specified in seconds.  The time series is specified
-% in volumes (TRs).  We need to do something about this, no?
 
 meanTS = mean(ts1(:));
 ts1 = 100* ((ts1 - meanTS)/ meanTS);
@@ -160,74 +169,90 @@ ylabel('Percent modulation of BOLD signal')
 grid on
 
 
-%%
-% % We can incorporate the knowlege of this hemodynamic response function
-% % into our design matrix by "convolving" each event with the hrf. This
-% % means that rather than being represented by a brief spike, the regressors
-% % are now smoothed in time to match the typical hemodynamic response
-% X(:,1) = conv(X(:,1), hrf, 'same');
-% X(:,2) = conv(X(:,2), hrf, 'same');
-% 
-% % Example of problem with conv
-% test = [0 0 0 0 1 0 0 0 0]
-% hrf  = [1 2 3]
-% conv(test,hrf,'same')
-% 
-% % Notice that the convolution with the hrf shifts to start at the 4th
-% % position, while the event doesn't begin until the 5th position.
-% % Without the 'same' flag the response starts at the right place
-% conv(test,hrf)
+%% Convolving the design matrix with the HRF (sorry for the jargon)
+%
+% We can incorporate the knowlege of this hemodynamic response function
+% into our design matrix by "convolving" each event with the hrf. This
+% means that rather than being represented by a brief spike, the regressors
+% are now smoothed in time to match the typical hemodynamic response. It is
+% not important that you understand this specific code, just the idea that
+% each event is multiplied or convolved with the known hemodynamic response
+% function (HRF)
 
-% So, let's do this
 tmp = conv(X(:,1),hrf); X(:,1) = tmp(1:length(X(:,1)));
 tmp = conv(X(:,2),hrf); X(:,2) = tmp(1:length(X(:,2)));
 
 % Now notice that the events in the design matrix are smoothed in time
 % reflecting the predicted hrf.
 figure; 
+
+% Plot the new predicted time series after convolution
 subplot(2,1,1)
 plot(X(:,1),'-r'); hold on
 plot(X(:,2),'-b')
 legend('words', 'scramble')
-
+% Show an image of the new design matrix
 subplot(2,1,2)
-imagesc(X); colormap('gray'); ylabel('Volume Number'); 
-set(gca, 'xtick',[1 2],'xticklabel', {'word' 'scramble'});
+imagesc(X'); colormap('gray'); xlabel('Volume Number'); 
+set(gca, 'ytick',[1 2],'yticklabel', {'word' 'scramble'});
 
 
 %% Fitting the linear model to the time series
 
+% In fMRI a linear model is used to estimate how much each event type
+% effects the measured BOLD signal. Another way to say this is that we want
+% to find the best way to scale our predictor matrix X to match our
+% measured signal. The scaling factor or weights for each condition are
+% related to the amount of BOLD signal modulation produced by that event.
+% Let's look at our predictors compare to our measured signal in this
+% voxel.
+figure; 
+subplot(2,1,1)
+plot(1:nTR,ts1)
+subplot(2,1,2)
+plot(X(:,1),'-r'); hold on
+plot(X(:,2),'-b')
+legend('words', 'scramble')
+
 % There is one more step before we fit a linear model to predict our
 % measured BOLD signal based on the regressors we created around our
-% experimental design. The units of the measured signal are arbitrary. We
-% are interested in predicting changes in the signal over time but we do
-% not care about the mean value of the time series. There are 2 ways to
-% deal with this. Either subtract the mean from the signal, or add a column
-% of ones to the design matrix before fitting the linear model. These two
-% aproaches are equivalent
-
-% Jason - demeaning like this is different from the usual detrending.  The
-% latter means pulling out the low frequency approximation.  Not sure it
-% matters.
-ts1_demeaned = ts1 - mean(ts1);
+% experimental design. As we said above the units of the measured signal
+% are arbitrary. We are interested in predicting changes in the signal over
+% time but we do not care about the mean value of the time series or other
+% aribtrary factors related to the scanner. To deal with this we include
+% regressors in the design matrix to deal with nuisance factors such as the
+% arbitraryness of the units and slow drift in signal intensity due to
+% imperfections in the instrament. These are often referred to as nuisance
+% regressors. Most researchers will include a column of ones to deal with
+% the offset and a linear term to deal with scanner drift.
+X = horzcat(X, ones(size(X,1),1), linspace(-1,1,size(X,1))');
+% Here is our new design matrix
+figure; imagesc(X);
 
 % We fit a linear model in which we scale each column of the
 % design matrix to best fit our measured signal.
 % Usually a linear model means you have a vector you wish to predict, in
 % this case the time series, as the weighted sum of other vectors in the
 % columns of a matrix, in this case the design matrix.  The weights are
-% often called 'beta' weights when we analyze fMRI data.
+% often called 'beta' weights when we analyze fMRI data. Here is the
+% equation for the linear model
 %
-%    ts1_demeaned = X * betaWeights
+%    ts1 = X * betaWeights
 %
 % Matlab solves these problems with the very simple formula
-B = X\ts1_demeaned;
+B = X\ts1;
 
 % This is the same as
-%   [B, B_ci] = regress(ts1_demeaned, X);
+%   B = regress(ts1_demeaned, X);
 % if you have the statistics toolbox
+%
+% The point to take away is that fitting a linear model to fMRI data is no
+% different than any regression probelm. We set up a matrix of predictor
+% variables and use any regression function to find weights that predict
+% our measurement as a weighted sum of our regressors.
 
-% These are the 'beta' weights for the word and scrambled word events
+% These are the 'beta' weights for the (1) word and (2) scrambled word
+% events, along with the weights on our nuisance variables.
 B
 
 %% Comparing the solution with the true data
@@ -238,18 +263,23 @@ B
 ts1_predicted = X*B;
 figure; hold
 plot(ts1_predicted,'-r')
-plot(ts1_demeaned,'-b')
+plot(ts1,'-b')
 
 
 %% Calculate the beta values for the whole slice
 % Now we can loop over all the voxels in this slice (z=10) and fit this
-% model to each voxel
+% model to each voxel. Even if this code doesn't make sense try to
+% apreciate the fact that we are fitting this same regression equation to
+% every voxel in this image. What we obtain is a set of 'beta' weights for
+% each voxel in the image. The resulting beta weight images show locations
+% where a given condition effects the BOLD signal
 
 % Allocate space
 B_words    = zeros(size(data,1),size(data,2));
 B_scramble = zeros(size(data,1),size(data,2));
 R2         = zeros(size(data,1),size(data,2));
-% First loop over the first dimension (the rows)
+
+% First loop over the first dimension of this slice (the rows)
 for ii = 1:size(data,1)
     % For each row we will now loop over the columns in that row
     for jj = 1:size(data,2)
@@ -264,29 +294,25 @@ for ii = 1:size(data,1)
         % and the weight for scrambled words and put it into another matrix
         B_words(ii,jj) = B(1);
         B_scramble(ii,jj) = B(2);
-        % Calculate the model prediction for this voxel
-        yhat = X*B;
-        % Calculate R squared (R2) the percent of variance explained by the
-        % model for this voxel
-        R2(ii,jj) = calculateR2(ts_demeaned, yhat);
     end
 end
 
 %% We can now visualize the beta weights as an image
+% This image shows how much each condition effected the BOLD signal in each
+% voxel of the image. Hence this map retains the same orientation as our
+% original data
 figure;
 imagesc(B_words);
+% Label the demensions of the axes
+xlabel ('Anterior <--> Posterior'); ylabel('Left <--> Right');
 % Add a colorbar
 colorbar
-% Scale the axis of the color bar to be from 0 to 150
-caxis([0 150]);
+% Scale the axis of the color bar to be from 0 to 30. This is roughly the
+% range of our beta weights
+caxis([0 30]);
 % Visualize the weights for the scrambled condition
 figure;
-imagesc(B_scramble); colorbar; caxis([0 150]);
-caxis([0 150]);
-
-%% What's this?
-figure
-imagesc(R2)
+imagesc(B_scramble); colorbar; caxis([0 30]);
 
 % Questions
 %
@@ -304,10 +330,16 @@ imagesc(R2)
 % to click on image pixels and get the x and y coordinates. You already
 % know the z coordinate (that is the slice number).
 
-
+%% Overlay the heatmap on a high resolution anatomical image
 % Typically it is nicer to look at an activation map overlaid over a high
 % resolution anatomical image. Lets load up a high resolution anatomical
 % that is in register with this fmri data
 inplane = niftiRead('Inplane.nii.gz');
+% This is what slice 10 of this image looks like. 
+figure;imagesc(inplane.data(:,:,10)); colormap('gray');
+
+% To overlay our activation map we must first interpolate it to be the same
+% resolution as our anatomy image which is 256x256 pixels
+map = imresize(B_words, [256 256]);
 
 
