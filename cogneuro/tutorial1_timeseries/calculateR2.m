c63117439edb2b3a126738e303b81b0b6da12d80
jyeatman <jyeatman@stanford.edu>
2013-04-15 15:39:04 -0700

    deleted a not needed function

--- cogneuro/tutorial1_timeseries/calculateR2.m
+++
@@ -1,9 +0,0 @@
-function R2 = calculateR2(y, yhat)
-% Calculate R2 for a model
-% R2 = calculateR2(y, yhat)
-%
-% Inputs:
-% y    - Data
-% yhat - Model prediction
-
-R2 = sum((y - yhat).^2) ./ sum((y - mean(y)).^2);
